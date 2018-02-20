ab6fa293cbd44837d12057be70b7ac2368ba8fef
java-2020 <java2020uk@gmail.com>
2018-02-20 08:08:12 +0000

    first mar commit!

---
+++ demoMar/src/main/java/com/example/mar/demoMar/DemoMarApplication.java
@@ -0,0 +1,12 @@
+package com.example.mar.demoMar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMarApplication.class, args);
+	}
+}
