7b37d42339d7f71d912d62891dc106526d15e4f7
java-2020 <java2020uk@gmail.com>
2018-02-20 08:06:05 +0000

    first jan commit!

---
+++ demoJan/src/main/java/com/example/jan/demoJan/DemoJanApplication.java
@@ -0,0 +1,13 @@
+package com.example.jan.demoJan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJanApplication.class, args);
+		System.out.println("def");
+	}
+}
